452982d365cf2d0a7c28820d424b7ba82bd22ea6
chrisbrasington <chrisbrasington@gmail.com>
2016-05-17 22:06:00 -0600

    connecting schedule data

--- program.rb
+++ program.rb
@@ -4,39 +4,80 @@ require 'net/http'
 require 'uri'
 require 'ap'
 
-#id = 0
-#
-#data = File.read('VehiclePosition.pb')
-#feed = Transit_realtime::FeedMessage.decode(data)
-#for e in feed.entity do
-#	#ap e
-#	print 'id: '
-#	puts e.vehicle.vehicle.id
-#	puts Time.at(e.vehicle.timestamp).strftime("%I:%M %p")
-#	ap e.id
-#	ap e.vehicle.position
-#	id = e.id 
-#	break
-#end
-#
-#data = File.read('TripUpdate.pb')
-#feed = Transit_realtime::FeedMessage.decode(data)
-#for entity in feed.entity do
-#	if entity.id = id
-#		print 'trip: '
-#		ap entity.trip_update.trip.trip_id
-#		entity.trip_update.stop_time_update.each do |trip|
-#			print 'stop_id: '
-#			print trip.stop_id
-#			print ' arrival: '
-#			print Time.at(trip.arrival.time).strftime("%I:%M %p")
-#			print ' departure: '
-#			puts Time.at(trip.departure.time).strftime("%I:%M %p")
-#			
-#		end
-#		
-#		break
-#	end
-#end
+def print_separator
+	puts '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+end
 
-puts '~~~~~~~~'
+def find_in_data (data, search_string, contains=true)
+	lines = []
+
+	data.each_line do |line|
+		if contains && line.include?(search_string)
+			lines.push(line)
+		elsif line.start_with?(search_string)
+			lines.push(line)
+		end
+	end
+	return lines
+end
+
+class DynamicDataList
+	attr_accessor :list
+	def initialize(data, schema)
+		@list = []
+		schema = schema.delete!("\r\n")
+		data.each do |route|		
+			route = route.split(',')	
+			hash = Hash.new
+			count = 0
+			schema.split(',').each do |property|
+				hash[property] = route[count]
+				count += 1
+			end
+			@list.append(hash)	
+		end
+	end
+	def to_s
+		ap @list
+	end
+end
+
+search_route = 'FF3'
+search_stop = 'US 36 & Sheridan Station Gate C'
+search_destination = 'Union Station'
+
+# find route information
+data = File.read('schedule/routes.txt')
+route_data = find_in_data(data, search_route, false)
+routes = DynamicDataList.new(route_data, data.lines.first)
+ap routes.list
+
+# find stop information
+data = File.read('schedule/stops.txt')
+stop_data = find_in_data(data, search_stop)
+stops = DynamicDataList.new(stop_data, data.lines.first)
+ap stops.list
+
+# find trip information to destination
+data = File.read('schedule/trips.txt')
+trip_data = find_in_data(data, search_route)
+trips = DynamicDataList.new(trip_data, data.lines.first)
+trips.list.delete_if do |trip|
+	if trip['trip_headsign'] != search_destination
+		true
+	end
+end
+
+trip_ids = [] 
+trips.list.each{ |t| trip_ids.push(t['trip_id'])}
+
+# find stop_time information
+data = File.read('schedule/stop_times.txt')
+stop_time_data = find_in_data(data, trip_ids.first, false)
+stop_times = DynamicDataList.new(stop_time_data, data.lines.first)
+
+print 'trip_id: ', trip_ids.first, "\n"
+stop_times.list.each{|s| 
+	print 'stop_id: ', s['stop_id'], "\n  "
+	print s['arrival_time'], '-', s['departure_time'], "\n" 
+}
