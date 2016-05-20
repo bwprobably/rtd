d20fd1eaadb7eb752742eee9008c0c3073d5318f
chrisbrasington <chrisbrasington@gmail.com>
2016-05-20 16:52:23 -0600

    connecting live data

--- program.rb
+++ program.rb
@@ -21,45 +21,24 @@ for e in feed.entity do
   if !defined?(e.vehicle.trip.route_id).nil?
     route_id = e.vehicle.trip.route_id
     if routes.has_key?(route_id)
-      routes[route_id].append(e.vehicle.trip)
+      routes[route_id].append(e)
     else
       routes[route_id] = []
-      routes[route_id].append(e.vehicle.trip)
+      routes[route_id].append(e)
     end
   end
-
 end
 
-routes['FF1'].select{|trip| trip.direction_id = 0}.each do |trip|
-  puts trip.trip_id
+updates = Hash.new
+data = File.read('realtime/TripUpdate.pb')
+feed = Transit_realtime::FeedMessage.decode(data)
+for e in feed.entity do
+  updates[e.trip_update.trip.trip_id] = e
 end
 
+routes['92'].select{|r| r.vehicle.trip.direction_id = 0}.each do |r|
+  trip_id = r.vehicle.trip.trip_id
+  puts "trip_id: #{trip_id}"
+  puts updates[trip_id].trip_update.stop_time_update[0].stop_id
+end
 
-# data = File.read('realtime2/TripUpdate.pb')
-# feed = Transit_realtime::FeedMessage.decode(data)
-#
-# routes = Hash.new
-# for e in feed.entity do
-#   route_id = e.trip_update.trip.route_id
-#
-#   if routes.has_key?(route_id)
-# 		routes[route_id] += 1
-# 	else
-#     routes[route_id] = 1
-#   end
-#
-#
-#
-#   #
-#   # if route_id == '101'
-#   #   trip_id = e.trip_update.trip.trip_id
-# 		# print 'route_id: ', route_id, "\n"
-#   #   print 'trip_id: ', trip_id, "\n"
-# 		# stop_id = e.trip_update.stop_time_update[0].stop_id
-#   #   arrival = Time.at(e.trip_update.stop_time_update[0].arrival.time)
-#   #   print 'stop_id: ', stop_id, "\n"
-# 		# print 'arrival: ', arrival, "\n"
-#   #   break
-#   # end
-# end
-# ap routes
