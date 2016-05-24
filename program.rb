0c35119c679a705c0e22b311bf4440aeb06a9946
chrisbrasington <chrisbrasington@gmail.com>
2016-05-23 20:47:56 -0600

    link live vehicle id to trip_updates

--- program.rb
+++ program.rb
@@ -7,22 +7,39 @@ require "sqlite3"
 
 $db = SQLite3::Database.open 'schedule.db'
 
-
-
-# trips = "select trips.trip_id from routes
-# 	inner join trips on trips.route_id = routes.route_id
-# 	where routes.route_id = 'FF3'
-# 	and (trips.service_id = 'MT' or trips.service_id = 'FR' or trips.service_id = 'WK')
-# 	order by trips.trip_id;"
-# trips = db.execute(trips)
-
-def stop_info(stop_id)
+# get stop info from scheduling data
+def get_stop_info(stop_id)
   return $db.execute("select * from stops where stop_id = #{stop_id}")[0]
+end
+
+# get trip update if existing for vehicle
+def get_updates(v, trips)
+  vehicle_id = v.vehicle.vehicle.id
+  route_id = v.vehicle.trip.route_id
+  trip_id = v.vehicle.trip.trip_id
+
+  trips[route_id].each { |t|
+    if !t.trip_update.nil? && t.trip_update.vehicle && t.trip_update.vehicle.id == vehicle_id
+      trip_count = 0
+
+      t.trip_update.stop_time_update.each{|u|
+        stop_id = u.stop_id
+        puts "   Stop: #{get_stop_info(stop_id)[1]}"
+        puts "     Arrival: #{Time.at(u.arrival.time).strftime("%l:%M%p %m-%e-%y ")}"
+        trip_count += 1
+
+        if trip_count >= 2
+          break
+        end
+      }
+    end
+  }
 end
 
 vehicleFile = 'realtime/VehiclePosition.pb'
 tripFile = 'realtime/TripUpdate.pb'
 
+# parse vehicle positioning
 vehicles = Hash.new
 data = File.read(vehicleFile)
 feed = Transit_realtime::FeedMessage.decode(data)
@@ -36,8 +53,7 @@ for e in feed.entity do
   end
 end
 
-# ap vehicles.keys.sort_by(&:downcase)
-
+# parse trip updates
 trip_updates = Hash.new
 data = File.read(tripFile)
 feed = Transit_realtime::FeedMessage.decode(data)
@@ -55,7 +71,6 @@ end
 search_route = 'FF3'
 if vehicles.keys.include?(search_route) && trip_updates.keys.include?(search_route)
   puts "Found #{search_route}: Vehicles: #{vehicles[search_route].count}, Trips: #{trip_updates[search_route].count}"
-
   count = 0
   vehicles[search_route].each { |v|
     stop_id = v.vehicle.stop_id
@@ -63,11 +78,11 @@ if vehicles.keys.include?(search_route) && trip_updates.keys.include?(search_rou
     puts "vehicle id: #{v.id} (#{v.vehicle.vehicle.id})"
     puts "   trip_id: #{v.vehicle.trip.trip_id}"
     # puts "   direction_id: #{v.vehicle.trip.direction_id}"
-    puts "   Stop: #{stop_info(stop_id)[1]}"
+    #puts "   Stop: #{get_stop_info(stop_id)[1]}"
     puts "   gps: #{v.vehicle.position.latitude},#{v.vehicle.position.longitude}"
     puts "   status: #{v.vehicle.current_status}"
+    get_updates(v, trip_updates)
     count += 1
-    exit
   }
 end
 
