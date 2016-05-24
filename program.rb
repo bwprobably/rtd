6a090da730f37d4c3db8d166febc80d597e6cd4f
chrisbrasington <chrisbrasington@gmail.com>
2016-05-23 19:09:45 -0600

    parsing live data..

--- program.rb
+++ program.rb
@@ -5,7 +5,9 @@ require 'uri'
 require 'ap'
 require "sqlite3"
 
-db = SQLite3::Database.open 'schedule.db'
+$db = SQLite3::Database.open 'schedule.db'
+
+
 
 # trips = "select trips.trip_id from routes
 # 	inner join trips on trips.route_id = routes.route_id
@@ -14,47 +16,70 @@ db = SQLite3::Database.open 'schedule.db'
 # 	order by trips.trip_id;"
 # trips = db.execute(trips)
 
-def stop_info(stop_id, db)
-  return db.execute("select * from stops where stop_id = #{stop_id}")
+def stop_info(stop_id)
+  return $db.execute("select * from stops where stop_id = #{stop_id}")[0]
 end
 
-routes = Hash.new
-data = File.read('realtime/VehiclePosition.pb')
+vehicleFile = 'realtime/VehiclePosition.pb'
+tripFile = 'realtime/TripUpdate.pb'
+
+vehicles = Hash.new
+data = File.read(vehicleFile)
 feed = Transit_realtime::FeedMessage.decode(data)
 for e in feed.entity do
-  if !defined?(e.vehicle.trip.route_id).nil?
+  if defined?(e.vehicle.trip.route_id)
     route_id = e.vehicle.trip.route_id
-    if routes.has_key?(route_id)
-      routes[route_id].append(e)
-    else
-      routes[route_id] = []
-      routes[route_id].append(e)
+    if !vehicles.has_key?(route_id)
+      vehicles[route_id] = []
     end
+    vehicles[route_id].append(e)
   end
 end
 
-updates = Hash.new
-data = File.read('realtime/TripUpdate.pb')
+# ap vehicles.keys.sort_by(&:downcase)
+
+trip_updates = Hash.new
+data = File.read(tripFile)
 feed = Transit_realtime::FeedMessage.decode(data)
 for e in feed.entity do
-  updates[e.trip_update.trip.trip_id] = e
-end
-
-route_id = '92'
-routes[route_id].select{|r| r.vehicle.trip.direction_id = 0}.each do |r|
-  trip_id = r.vehicle.trip.trip_id
-  puts "route_id:\t#{route_id}"
-  puts "trip_id:\t#{trip_id}"
-  if !updates[trip_id].nil?
-    vehicle_id = r.vehicle.vehicle.id
-    stop_id = updates[trip_id].trip_update.stop_time_update[0].stop_id
-    arrival = Time.at(updates[trip_id].trip_update.stop_time_update[0].arrival.time)
-    stop_info = stop_info(stop_id, db)
-
-    puts "vehicle_id:\t#{vehicle_id}"
-    puts "stop:\t\t#{stop_info[0][1]}"
-    puts "arrival:\t#{arrival.strftime("%m-%e-%y %l:%M%p")}"
-    puts '~~~~~~~~~~~~~~~~~~~~~~'
+  if defined?(e.trip_update.trip.route_id)
+    route_id = e.trip_update.trip.route_id
+    if !trip_updates.has_key?(route_id)
+      trip_updates[route_id] = []
+    end
+    trip_updates[route_id].append(e)
   end
 end
+
+
+search_route = 'FF3'
+if vehicles.keys.include?(search_route) && trip_updates.keys.include?(search_route)
+  puts "Found #{search_route}: Vehicles: #{vehicles[search_route].count}, Trips: #{trip_updates[search_route].count}"
+
+  count = 0
+  vehicles[search_route].each { |v|
+    stop_id = v.vehicle.stop_id
+    print "[#{count}] "
+    puts "vehicle id: #{v.id} (#{v.vehicle.vehicle.id})"
+    puts "   trip_id: #{v.vehicle.trip.trip_id}"
+    # puts "   direction_id: #{v.vehicle.trip.direction_id}"
+    puts "   Stop: #{stop_info(stop_id)[1]}"
+    puts "   gps: #{v.vehicle.position.latitude},#{v.vehicle.position.longitude}"
+    puts "   status: #{v.vehicle.current_status}"
+    count += 1
+    exit
+  }
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
