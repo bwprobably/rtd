a7041aa4876d4bad238db3ee6eb5788d3eeddd89
chrisbrasington <chrisbrasington@gmail.com>
2016-06-08 19:14:33 -0600

    settings class

--- program.rb
+++ program.rb
@@ -6,106 +6,54 @@ require 'awesome_print'
 require 'yaml'
 require "./schedule"
 require "./live_data"
+require "./settings"
 
-schedule = Schedule.new
-live_data = Live_Data.new
-settings = Live_Data.new
-
-exit
-
+# convert time to readable format
 def time_to_str(time)
   time = time.split(':')
   hours = time[0].to_i
   min = time[1]
   am_pm = 'AM'
-
   if(hours > 12)
     am_pm = 'PM'
     hours -= 12
   end
-
   return "#{hours}:#{min}#{am_pm}"
-
 end
 
-def print_vehicle_info(v, count)
-  v.each{
-      |p|
-
-
-
+# print vehicle information
+def print_vehicle_info(v)
+  count = 0
+  v.each{ |p|
     if count == 2 #time
       printf "%-4s ", time_to_str(p)
-    elsif count == 0 #trip_id
-      # printf "(%-4s) ", p
-    else
+    # elsif count == 0 #trip_id
+    #   # printf "(%-4s) ", p
+    #   printf ""
+    elsif count != 0
       printf "%-4s ", p
     end
     count += 1
   }
 end
 
-
-# load settings
-fullPath = "./"
-settings = YAML.load_file(fullPath+'settings.yml')
-$favorite_routes = settings['favorites'].split(',')
+schedule = Schedule.new
+live_data = Live_Data.new
+settings = Settings.new
 
 prior_time = ''
-settings['morning'].each{|s|
-
-  # parse settings
-  from = s[1]['from']
-  to = s[1]['to']
-  dir = s[1]['direction']
-  time = s[1]['time']
-  type = s[1]['type']
-
-  # if !time.nil?
-  #   time = current_time
-  # end
-
-  if time.nil?
-    time_after = s[1]['time_after']
-    time = prior_time + time_after*60 #time in minutes
-    prior_time = time
-  else
-    prior_time = time
-  end
-
-
-
-
-
-
-  case dir
-    when 'South'
-      dir = '1'
-    when 'North'
-      dir = '0'
-    when 'West'
-      dir = '1'
-    when 'East'
-      dir = '0'
-  end
+settings.list['morning'].each{|s|
+  setting = settings.parse_setting(s, prior_time)
+  prior_time = setting.time
 
   # checking...
-  puts "'#{from}' to '#{to}' at ~#{time_to_str(time.strftime("%H:%M"))}"
-
-  # next
-
-
-
-
-
-
-
+  puts "'#{setting.from}' to '#{setting.to}' at ~#{time_to_str(setting.time.strftime("%H:%M"))}"
 
   # get stop(s) for starting point
   # this is a really inefficient way to do this
   # I shouldn't be checking all stops, but only stops involved in my trip's destination
   # Little tricky to ask
-  stops = schedule.get_stop_info_by_name(from, dir)
+  stops = schedule.get_stop_info_by_name(setting.from, setting.dir)
 
   vehicles = []
 
@@ -113,113 +61,84 @@ settings['morning'].each{|s|
     stop_id = s[0]
 
     # get trips near time
-    trips = schedule.get_trips_near_time(stop_id, time, type)
+    trips = schedule.get_trips_near_time(stop_id, setting.time, setting.type)
 
+    # for each tri[]
     trips.each{ |t|
 
+      # get trip info
       trip_id = t[0]
       arrival_time = t[1]
-
       trip_info = schedule.get_trip_info(trip_id)
       route_id = trip_info[0]
 
-      if dir == trip_info[4] and $favorite_routes.include?(route_id)
+      # if going the direction we're going and the route includes a favorited stop
+      if setting.dir == trip_info[4] and settings.favorites.include?(route_id)
 
-        result = schedule.heading_to_destination?(trip_id, to, dir)
+        # check if actually heading to destination
+        result = schedule.heading_to_destination?(trip_id, setting.to, setting.dir)
 
+        # trip is scheduled to our destination
         if !result[0].nil?
-          # trip_info = schedule.get_trip_info(trip_id)
-          # route_id = trip_info[0]
           day = trip_info[1];
 
+          # add to vehicles found
           if day != 'SA' and day != 'SU' and day != 'FR'
-            # printf "(#{trip_id}) %-5s %s #{day}\n", route_id, arrival_time[0..-4]
             vehicles.append([trip_id, route_id, arrival_time[0..-4], day])
           end
-
         end
-
       end
     }
   }
 
+  # sort vehicles sort by arrival time
   vehicles = vehicles.sort_by{|v| v[2]}
 
+  # loop each vehcile
   vehicles.each{|v|
     trip_id = v[0]
-    count = 0
 
-    if type == 'train' or type == 'bus'
-      print_vehicle_info(v, count)
+    # print information
+    if setting.type == 'train'
+      print_vehicle_info(v)
       puts
     end
 
-
-    if type == 'bus' and !live_data.trip_updates[trip_id].nil?
+    # if bus has live data, append live data
+    if setting.type == 'bus' and !live_data.trip_updates[trip_id].nil?
       v_id = live_data.trip_updates[trip_id][0]['vehicle']['label']
       time_stamp = live_data.trip_updates[trip_id][0]['vehicle']['timestamp']
 
       count = live_data.vehicle_updates[trip_id][0]['trip_update']['stop_time_update'].size
       sequence = live_data.vehicle_updates[trip_id][0]['trip_update']['stop_time_update'][0]['stop_sequence']
-      last_sequnce = live_data.vehicle_updates[trip_id][0]['trip_update']['stop_time_update'][count-1]['stop_sequence']
-
+      last_sequence = live_data.vehicle_updates[trip_id][0]['trip_update']['stop_time_update'][count-1]['stop_sequence']
 
       stop_id = live_data.vehicle_updates[trip_id][0]['trip_update']['stop_time_update'][0]['stop_id']
       stop_info = schedule.get_stop_info_by_id(stop_id)
       stop_name = stop_info[1]
 
-      if stop_name.include?(to)
+      # skip if live bus data has passed our destination
+      if stop_name.include?(setting.to)
         next
         # puts "\n    PAST STOP"
       end
 
+      print_vehicle_info(v)
 
-      print_vehicle_info(v, count)
-
-
-      # v.each{
-      #     |p|
-      #
-      #   if count == 2
-      #     printf "%-4s ", p
-      #   elsif count == 0
-      #     printf "(%-4s) ", p
-      #   else
-      #     printf "%-4s ", p
-      #   end
-      #   count += 1
-      # }
+      # live data found, print
       print "\n    LIVE: #{Time.at(time_stamp).strftime("%l:%M%p")} "
-      print "(#{sequence}/#{last_sequnce}) "
+      print "(#{sequence}/#{last_sequence}) "
       print stop_name
 
-      if stop_name.include?(from)
+      # bus is at stop
+      if stop_name.include?(setting.from)
         print "\n    CURRENTLY AT STOP"
       end
       puts
-
-
-
-
-
-
-
-
+    elsif setting.type == 'bus'
+      print_vehicle_info(v)
+      puts
     end
-
-
-
-    # exit
-    # puts
   }
-
-  puts
+  puts "\n"
 }
-
-
-
-
-
-
-
-
--- settings.rb
+++ settings.rb
@@ -1,12 +1,56 @@
 class Settings
 
-  attr_accessor :favorites
+  attr_accessor :favorites, :list
 
   def initialize
     fullPath = "./"
     settings = YAML.load_file(fullPath+'settings.yml')
     @favorites = settings['favorites'].split(',')
-
-    ap settings
+    @list = settings
   end
+
+  def parse_setting(s, prior_time)
+    # parse settings
+    from = s[1]['from']
+    to = s[1]['to']
+    dir = s[1]['direction']
+    time = s[1]['time']
+    type = s[1]['type']
+
+    if time.nil?
+      time_after = s[1]['time_after']
+      time = prior_time + time_after*60 #time in minutes
+      prior_time = time
+    else
+      prior_time = time
+    end
+
+    case dir
+      when 'South'
+        dir = '1'
+      when 'North'
+        dir = '0'
+      when 'West'
+        dir = '1'
+      when 'East'
+        dir = '0'
+    end
+
+    return Setting.new(to, from, dir, time, type)
+
+  end
+end
+
+class Setting
+  attr_accessor :to, :from, :dir, :time, :type
+
+  def initialize(to, from, dir, time, type)
+    @to = to
+    @from = from
+    @dir = dir
+    @time = time
+    @type = type
+
+  end
+
 end
